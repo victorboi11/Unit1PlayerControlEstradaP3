fae6722e8a821b89a4db65f002b5a25eaa21cc97
victorboi11 <725304@student.dusd.net>
2024-04-24 12:14:41 -0700

    finished this
    yaaay

--- Prototype1EstradaP3/Assets/Course Library/Scripts/PlayerController.cs
+++ Prototype1EstradaP3/Assets/Course Library/Scripts/PlayerController.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 5.0f;
-    private float turnSpeed;
+    // Private Variables
+    private float speed = 15.0f;
+    private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float fowardInput;
     // Start is called before the first frame update
@@ -17,11 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // This is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
         fowardInput = Input.GetAxis("Vertical");
-        // Moves the car foward based on vertical input
+
+        // We move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
-        // Rotates the car based on horizontal input
+        // We turn the vehicle
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
